61c15f5fd46bb913e658200ab31a0d6fe57677ee
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2021-12-05 03:05:37 +0100

    feat(reservation-agence): ajout du WS avec la notion de réservatoin en agence

--- reservation-back-web/src/main/java/mg/hotel/reservation/configuration/WebServiceConfiguration.java
+++ reservation-back-web/src/main/java/mg/hotel/reservation/configuration/WebServiceConfiguration.java
@@ -1,5 +1,6 @@
 package mg.hotel.reservation.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +26,7 @@ public class WebServiceConfiguration extends WsConfigurerAdapter {
     }
 
     @Bean(name = "reservationsWsdl")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema reservationSchema) {
+    public DefaultWsdl11Definition reservationWsdl11Definition(@Qualifier("reservationXsdSchema") XsdSchema reservationSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("ReservationPort");
         wsdl11Definition.setLocationUri("/ws/reservations");
@@ -34,8 +35,23 @@ public class WebServiceConfiguration extends WsConfigurerAdapter {
         return wsdl11Definition;
     }
 
-    @Bean
+    @Bean(name = "reservationsAgenceWsdl")
+    public DefaultWsdl11Definition reservationAgenceWsdl11Definition(@Qualifier("reservationAgenceXsdSchema") XsdSchema reservationAgenceSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("ReservationAgencePort");
+        wsdl11Definition.setLocationUri("/ws/reservations-agence");
+        wsdl11Definition.setTargetNamespace("http://hotel.mg/reservation-agence");
+        wsdl11Definition.setSchema(reservationAgenceSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean("reservationXsdSchema")
     public XsdSchema reservationSchema() {
-        return new SimpleXsdSchema(new ClassPathResource("reservations.xsd"));
+        return new SimpleXsdSchema(new ClassPathResource("wsdl/reservations.xsd"));
+    }
+
+    @Bean("reservationAgenceXsdSchema")
+    public XsdSchema reservationAgenceSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("wsdl/reservations-agence.xsd"));
     }
 }
--- reservation-back-web/src/main/java/mg/hotel/reservation/services/HotelService.java
+++ reservation-back-web/src/main/java/mg/hotel/reservation/services/HotelService.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import mg.hotel.reservation.Chambre;
 import mg.hotel.reservation.Hotel;
 import mg.hotel.reservation.ProcessReservationRequest;
 import mg.hotel.reservation.ProcessReservationResponse;
+import mg.hotel.reservation_agence.CheckDisponibiliteParAgenceRequest;
+import mg.hotel.reservation_agence.CheckDisponibiliteParAgenceResponse;
+import mg.hotel.reservation_agence.ProcessReservationAgenceRequest;
+import mg.hotel.reservation_agence.ProcessReservationAgenceResponse;
 
 import static java.util.Collections.singletonList;
 
@@ -24,7 +29,7 @@ public class HotelService {
     public ProcessReservationResponse processReservation(ProcessReservationRequest processReservation) {
         if (isPayementValid(processReservation)) {
             ProcessReservationResponse response = new ProcessReservationResponse();
-            response.setNomHotel("Carlton");
+            response.setReferenceReservation("REF001");
             return response;
         }
         throw new RuntimeException("Payment non valide");
@@ -32,5 +37,25 @@ public class HotelService {
 
     private boolean isPayementValid(ProcessReservationRequest processReservation) {
         return processReservation.getPayement() != null && processReservation.getPayement().getDateFinValidite() != null && processReservation.getPayement().getDateFinValidite().isValid();
+    }
+
+    public CheckDisponibiliteParAgenceResponse checkDisponibiliteParAgence(CheckDisponibiliteParAgenceRequest request) {
+        CheckDisponibiliteParAgenceResponse response = new CheckDisponibiliteParAgenceResponse();
+        mg.hotel.reservation_agence.Hotel hotel = new mg.hotel.reservation_agence.Hotel();
+        hotel.setNom("Carlton");
+        mg.hotel.reservation_agence.Chambre chambre = new mg.hotel.reservation_agence.Chambre();
+        chambre.setReference("REF001CHAMBRE");
+        chambre.setReductionPartenariat(10);
+        chambre.setNombreLit((short) 4);
+        chambre.setPrix(150.25f);
+        hotel.getChambre().add(chambre);
+        response.setHotels(hotel);
+        return response;
+    }
+
+    public ProcessReservationAgenceResponse processReservationAgence(ProcessReservationAgenceRequest request) {
+        ProcessReservationAgenceResponse response = new ProcessReservationAgenceResponse();
+        response.setReferenceReservation("REF001PROMO");
+        return response;
     }
 }
