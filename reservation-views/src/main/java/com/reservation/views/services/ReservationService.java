e1bb310c48ca2ed3e0bc7c1b119a9fe3d527f895
Elie RASOANAIVO <el.rasoanaivo@gmail.com>
2021-12-05 23:09:52 +0100

    feat(SOAP-client): implement call service SOAP

--- reservation-views/src/main/java/com/reservation/views/services/ReservationService.java
+++ reservation-views/src/main/java/com/reservation/views/services/ReservationService.java
@@ -1,21 +1,36 @@
 package com.reservation.views.services;
 
-import org.springframework.stereotype.Service;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 import mg.reservation.reservationAgence.wsdl.CheckDisponibiliteParAgenceRequest;
+import mg.reservation.reservationAgence.wsdl.CheckDisponibiliteParAgenceResponse;
 import mg.reservation.reservationAgence.wsdl.Hotel;
 import mg.reservation.reservationAgence.wsdl.ProcessReservationAgenceRequest;
 import mg.reservation.reservationAgence.wsdl.ProcessReservationAgenceResponse;
 
-@Service
-public class ReservationService {
+public class ReservationService extends WebServiceGatewaySupport {
+
+    public static final String SOAP_URI = "http://localhost:8080/soap/ws/reservations-agence";
+    public static final String FIND_HOTEL_NAME_SPACE = "http://hotel.mg/reservation-agence/checkDisponibiliteParAgenceRequest";
+    public static final String FAIRE_RESERVATION_NAME_SPACE = "http://hotel.mg/reservation-agence/processReservationAgenceRequest";
+
+    public ReservationService() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("mg.reservation.reservationAgence.wsdl");
+        setMarshaller(marshaller);
+        setUnmarshaller(marshaller);
+    }
+
     public Hotel findPlaceDisponible(CheckDisponibiliteParAgenceRequest request) {
-        // todo implement
-        return new Hotel();
+        CheckDisponibiliteParAgenceResponse response = (CheckDisponibiliteParAgenceResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(SOAP_URI, request, new SoapActionCallback(FIND_HOTEL_NAME_SPACE));
+        return response.getHotels().get(0);
     }
 
     public ProcessReservationAgenceResponse faireReservation(ProcessReservationAgenceRequest request) {
-        // todo implement
-        return new ProcessReservationAgenceResponse();
+        return (ProcessReservationAgenceResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(SOAP_URI, request, new SoapActionCallback(FAIRE_RESERVATION_NAME_SPACE));
     }
 }
